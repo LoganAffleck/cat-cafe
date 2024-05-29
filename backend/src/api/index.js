fe109fad0d0a1fa57837ed785b0fb950a2e3a5bd
J. Logan Affleck <affleck.logan@gmail.com>
2024-05-28 22:20:08 -0400

    Update index.js

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,6 +1,6 @@
 require("./bin/www");
-const { PORT = 5000 } = process.env;
-const app = require("./app");
+// const { PORT = 5000 } = process.env;
+// const app = require("./app");
 
-const listener = () => console.log(`Listening on Port ${PORT}!`);
-app.listen(PORT, listener);
+// const listener = () => console.log(`Listening on Port ${PORT}!`);
+// app.listen(PORT, listener);
